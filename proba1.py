6d56e76e58edd06fb9775d21bd21d26a0581ef40
Viktorius98 <viksin376@yandex.ru>
2022-10-04 19:11:19 +0300

    python practica1

---
+++ proba1.py
@@ -0,0 +1,9 @@
+iter_obj = iter("Hello!")
+
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
