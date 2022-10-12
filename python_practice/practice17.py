7f049f926b8908c330a409d033c8b7425c3b6bc5
Viktorius98 <viksin376@yandex.ru>
2022-10-13 02:46:59 +0300

    python practica14

--- python_practice/practice17.py
+++ python_practice/practice17.py
@@ -1,12 +1,12 @@
-arroy = list(map(int, input('Введите любую последовательность чисел через пробел: ').split()))
+array = list(map(int, input('Введите любую последовательность чисел через пробел: ').split()))
 some_number = int(input('Введите любое число: '))
 
 while some_number:
-    if some_number <= min(arroy):
+    if some_number <= min(array):
         print('Введенное число не соответствует критериям поиска, введите число не превышающее максимальное значение и'
               'больше минимального')
         some_number = int(input('Введите любое число: '))
-    elif some_number > max(arroy):
+    elif some_number > max(array):
         print('Введенное число не соответствует критериям поиска, введите число не превышающее максимальное значение и'
               'больше минимального')
         some_number = int(input('Введите любое число: '))
@@ -26,12 +26,16 @@ def binary_search(array_1, some_number, left, right):
 
     if left > right:
         return False
-    middle = (right + left) // 2
+
+    middle = (right+left) // 2
     if array_1[middle] < some_number and array_1[middle + 1] >= some_number:
         return middle
     elif some_number < array_1[middle]:
-        return binary_search(array_1, some_number, left, middle -1)
+        return binary_search(array_1, some_number, left, middle-1)
     else:
-        return binary_search(array_1, some_number, middle + 1, right)
+        return binary_search(array_1, some_number, middle+1, right)
+
+some_number = int(input())
+array_1 = [i for i in range(0, len(array_1)-1)]
 
 print(binary_search(array_1, some_number, 0, len(array_1) - 1))
